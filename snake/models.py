7779bc61300a7d895c9a7c1d2c8fad7b758e1f6e
Willie Tran <tran.william26@gmail.com>
2014-08-04 15:01:54 -0700

    scores model setup

--- snake/models.py
+++ snake/models.py
@@ -1,6 +1,22 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, User
 from django.db import models
 
 # Create your models here.
 
 
+class Score(models.Model):
+    player = models.ForeignKey(User, related_name='user')
+    score = models.PositiveIntegerField()
+    date = models.DateTimeField(auto_now_add=True)
+
+    game_choices = (
+        ('S', 'Snake'),
+        ('M', 'Memory'),
+    )
+
+    game = models.CharField(max_length=1,
+                            choices=game_choices,
+                            default='S')
+
+    def __unicode__(self):
+        return u"{}".format(self.score)
